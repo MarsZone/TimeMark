ec923362eb3ce6bc868141cb7261f7cbc8566b9f
marszhang <zm03322161@qq.com>
2017-12-30 23:57:40 +0800

    Calendar
    123

--- src/pages/Calendar.vue
+++ src/pages/Calendar.vue
@@ -1,13 +1,51 @@
 <template>
   <v-ons-page>
     <div class="Calendar">
-      <div class="focus"></div>
+      <calendar-month
+        :show-date="showDate"
+        :events = "events"
+        @setShowDate="setShowDate"
+        class="holiday-us-traditional holiday-us-official"
+      />
     </div>
   </v-ons-page>
 </template>
 
 <script>
+  import CalendarMonth from "vue-simple-calendar";
+  require("vue-simple-calendar/dist/static/css/default.css");
+  require("vue-simple-calendar/dist/static/css/holidays-us.css");
+
   export default {
+
+    data: function() {
+      return {
+        showDate: new Date(),
+        locale : 'en',
+        events:[
+          {
+            id:'1',
+            title:'Study abc',
+            startDate:'2017-12-28',
+            endDate:'2017-12-28',
+          },
+          {
+            id:'2',
+            title:'Read a book',
+            startDate:'2017-12-5',
+            endDate:'2017-12-5',
+          }
+        ]
+      }
+    },
+    components: {
+      CalendarMonth
+    },
+    methods: {
+      setShowDate(d) {
+        this.showDate = d;
+      },
+    }
   };
 </script>
 
@@ -15,17 +53,8 @@
   .Calendar {
     width: 100%;
     height: 100%;
-    background-color: lightgrey;
-    vertical-align: middle;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    background-color: whitesmoke;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
   }
-  .focus {
-    width: 100px;
-    height: 100px;
-    border: 12px solid whitesmoke;
-    border-radius: 100%;
-  }
+
 </style>
