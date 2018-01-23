42399cfbd58b571cc927b8c3e83c212751e32b71
marszhang <zm03322161@qq.com>
2018-01-23 21:21:44 +0800

    Week Chart View

--- src/components/charts/Week.vue
+++ src/components/charts/Week.vue
@@ -2,7 +2,8 @@
   <v-ons-page>
     <custom-toolbar v-bind="toolbarInfo"></custom-toolbar>
     <div class="Charts">
-      <chart :options="options"></chart>
+      <chart
+        :options="seriesdata"></chart>
     </div>
   </v-ons-page>
 </template>
@@ -14,16 +15,61 @@
   import 'echarts/lib/component/tooltip'
   // built-in theme
   import 'echarts/theme/dark'
+  import axios from 'axios';
+  import Bus from '../../components/bus.js';
+
   export default {
     components: {
       chart: ECharts
     },
     data: function () {
-
       return {
+        chartData:[
+          {
+            name:'学习',
+            type:'line',
+            stack: 'Max',
+            areaStyle: {normal: {}},
+            data:[120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name:'运动',
+            type:'line',
+            stack: 'Max',
+            areaStyle: {normal: {}},
+            data:[220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name:'项目',
+            type:'line',
+            stack: 'Max',
+            areaStyle: {normal: {}},
+            data:[150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name:'默认',
+            type:'line',
+            stack: 'Max',
+            areaStyle: {normal: {}},
+            data:[320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name:'搜索引擎',
+            type:'line',
+            stack: 'Max',
+            label: {
+              normal: {
+                show: true,
+                position: 'top'
+              }
+            },
+            areaStyle: {normal: {}},
+            data:[120, 932, 901, 934, 1290, 1330, 1320]
+          }
+        ],
         options:{
           title: {
-            text: '一周统计'
+            text: 'Week View'
           },
           tooltip : {
             trigger: 'axis',
@@ -35,7 +81,7 @@
             }
           },
           legend: {
-            data:['学习','运动','项目','默认','搜索引擎']
+            data:['学习','运动','项目','默认','搜索引擎'],
           },
           toolbox: {
             feature: {
@@ -44,7 +90,7 @@
           },
           grid: {
             left: '1%',
-            right: '2%',
+            right: '1%',
             bottom: '1%',
             containLabel: true
           },
@@ -52,8 +98,7 @@
             {
               type : 'category',
               boundaryGap : false,
-
-              data : ['周一','周二','周三','周四','周五','周六','周日']
+              data : ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
             }
           ],
           yAxis : [
@@ -64,58 +109,38 @@
               }
             }
           ],
-          series : [
-            {
-              name:'学习',
-              type:'line',
-              stack: '总量',
-              areaStyle: {normal: {}},
-              data:[120, 132, 101, 134, 90, 230, 210]
-            },
-            {
-              name:'运动',
-              type:'line',
-              stack: '总量',
-              areaStyle: {normal: {}},
-              data:[220, 182, 191, 234, 290, 330, 310]
-            },
-            {
-              name:'项目',
-              type:'line',
-              stack: '总量',
-              areaStyle: {normal: {}},
-              data:[150, 232, 201, 154, 190, 330, 410]
-            },
-            {
-              name:'默认',
-              type:'line',
-              stack: '总量',
-              areaStyle: {normal: {}},
-              data:[320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-              name:'搜索引擎',
-              type:'line',
-              stack: '总量',
-              label: {
-                normal: {
-                  show: true,
-                  position: 'top'
-                }
-              },
-              areaStyle: {normal: {}},
-              data:[820, 932, 901, 934, 1290, 1330, 1320]
-            }
-          ]
-        }
+          series : []
+        },
+        toolbarInfo: {
+          backLabel: 'Chart',
+          title: 'WeekView'
+        },
       }
-    }
+    },
+    computed: {
+      seriesdata: function () {
+        this.options.series = this.chartData;
+        return this.options;
+      }
+    },
+    methods: {
+
+    },
   }
 </script>
 
 <style>
   .Charts {
     width: 100%;
-    height: 100%;
+    /*height: 100%;*/
+
+    -webkit-transform: rotateZ(90deg);
+    -moz-transform: rotateZ(90deg);
+    -o-transform: rotateZ(90deg);
+    -ms-transform: rotateZ(90deg);
+    transform: rotateZ(90deg);
+  }
+  .echarts{
+    transform:scaleY(0.9);
   }
 </style>
